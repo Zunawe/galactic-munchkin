ca64c1a94aa37bb2bed3ae3223bca4ba0ebccedb
Mason Salmon <Salmonmasons@aol.com>
2018-02-24 14:31:16 -0700

    this commit reads txt lines?

--- munch.js
+++ munch.js
@@ -1,3 +1,21 @@
+// preload
+document.getElementById('textf.txt').onchange = function(){
+
+  var file = this.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function(progressEvent){
+    // Entire file
+    console.log(this.result);
+
+    // By lines
+    var lines = this.result.split('\n');
+    for(var line = 0; line < lines.length; line++){
+      console.log(lines[line]);
+    }
+  };
+  reader.readAsText(file);
+};
 // Card Function def:
 
 function Card(fields)

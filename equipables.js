594c47adf364b95bcbf816b069aef826c7eaba01
Eric Speaker <ersp4587@colorado.edu>
2018-02-24 14:43:41 -0700

    Add files via upload

---
+++ equipables.js
@@ -0,0 +1,20 @@
+//for items allowed the play is allowed to hold
+
+function Equipable(fields){
+	_this = this;
+	_this.head = fields.head;
+	_this.armor = field.level;
+	_this.lhand = fields.lhand;
+	_this.rhand = fields.rhand;
+	_this.foot = fields.foot;
+	_this.other = fields.other;
+//not sure about the fields ^^ in this file
+
+//head
+//armor
+//hand
+//hand
+//foot
+//other
+
+}
